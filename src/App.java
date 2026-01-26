6e75a687b70adf199a9b72b6c10853210818a04e
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2026-01-26 13:00:22 +0100

    init prgramm, skrivit "Todo" list

---
+++ src/App.java
@@ -0,0 +1,28 @@
+void main(){
+
+/* 
+    klasser
+        Kund
+        Bokning 
+            Bil
+            Flyg
+            Båt? 
+    
+    array med alla passagerar 
+    personummer (namn)?
+    pris 
+    visa grafiskt
+    
+    Meny
+        lägg till 
+        antal lediga platser 
+        beräkna vinst beroende på typ och pris 
+        hitta bokning (personummer eller namn)
+        ta bort bokning 
+        skriv ut alla platser sorterat på ålder 
+
+
+*/
+
+    System.out.println("Hello, World!");
+}
